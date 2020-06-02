b631f08084f85a4ac9afae4d9630524eddb8aa95
Fal Alexander <falalexandr007@gmail.com>
2020-06-02 21:44:25 +0700

    Extracted constants

--- gcp_deploy.bat
+++ gcp_deploy.bat
@@ -1,1 +1,1 @@
-gcloud config set project %1 && gcloud beta functions deploy handle --region=europe-west1 --runtime python37 --trigger-http --memory=128MB --set-env-vars=TOKEN=%2
+gcloud config set project %1 && gcloud beta functions deploy webHook --entry-point handle --region=europe-west1 --runtime python37 --trigger-http --memory=128MB --set-env-vars=TOKEN=%2
--- toxic_bot.py
+++ toxic_bot.py
@@ -1,20 +1,27 @@
-import os
 import telebot
+import toxic_bot_constants as const
 
-project_id_ = os.environ['GCP_PROJECT']
-entry_point_ = os.environ['ENTRY_POINT']
-function_region_ = os.environ['FUNCTION_REGION']
-url_ = f'https://{function_region_}-{project_id_}.cloudfunctions.net/{entry_point_}'
-
-bot = telebot.TeleBot(os.environ['TOKEN'])
-bot.set_webhook(url_)
+bot = telebot.TeleBot(const.TOKEN)
+bot.set_webhook(const.URL)
 
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Здарова, Бандит!')
+def handle_start(message):
+    if message.chat.type == 'private' and message.chat.username is not None and message.chat.username == 'faleksander1':
+        bot.reply_to(message, 'Здарова, Бандит!')
+        bot.send_sticker(message.chat.id, const.FRY_FUCK_YOU_STICKER, message.message_id)
+    elif message.chat.last_name == 'Fal' or message.chat.last_name == 'Кузьмин':
+        bot.reply_to(message, 'Падла')
+    else:
+        bot.send_sticker(message.chat.id, const.FRY_FUCK_YOU_STICKER, message.message_id)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def handle_message(message):
+    bot.reply_to(message, message.text)
 
 
 def process(json):
-    bot.process_new_messages([telebot.types.Update.de_json(json).message])
-
+    de_json = telebot.types.Update.de_json(json)
+    if de_json is not None:
+        bot.process_new_messages([de_json.message])
